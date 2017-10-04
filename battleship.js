9078c01d6d1382c360d158d617e39ef52a6dfb82
StefanHaslhofer <32057772+StefanHaslhofer@users.noreply.github.com>
2017-10-04 17:07:48 +0200

    Add files via upload

--- battleship.js
+++ battleship.js
@@ -1,12 +1,14 @@
 $(() => {
   // Select table containing the battleground
   const battleground = $('#battleground');
+  let upper=10;
+  let lower=0;
 
   // Build 10 x 10 grid for battleground
-  for (let row = 0; row < 10; row++) {
+  for (let row = 0; row < upper; row++) {
     // Create table row
     const tr = $('<tr>');
-    for (let column = 0; column < 10; column++) {
+    for (let column = 0; column < upper; column++) {
       // Create table cell with CSS class `water`. Note that we use
       // HTML data attributes  to store the coordinates of each cell
       // (see https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes). 
@@ -20,11 +22,46 @@ $(() => {
 
   $('#generate').click(() => {
     // Here you have to add your code for building a random battleground.
-
-    // Tip: The next line of code demonstrates how you can select a table cell
-    // using coordinates, remove CSS classes and add CSS classes. 
-    $('td[data-r="1"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="2"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="3"][data-c="1"]').removeClass('water').addClass('ship');
+    var ships = [2, 3, 3, 4, 5];
+    var count=0;
+    for(let i=0;i<ships.length;){
+      if(placeShip(ships[i])){
+        i++;
+      }
+      count++;
+      if(count>1000){
+        throw new Error("No possible formation!");
+      }
+    }
   });
 });
+
+function placeShip(size){
+  
+  var direction=Math.floor(Math.random()*(2-1+1)+1);
+  var x=Math.floor(Math.random()*((10-size)-0+1));
+  var y=Math.floor(Math.random()*((10-size)-0+1));
+
+  if(direction==1){
+    for(let i=x-1;i<x+size+1;i++){
+      if($('td[data-r="'+(y+1)+'"][data-c="'+i+'"]').hasClass('ship')||$('td[data-r="'+(y-1)+'"][data-c="'+i+'"]').hasClass('ship')||$('td[data-r="'+y+'"][data-c="'+i+'"]').hasClass('ship')){
+        return false;
+      }
+    }
+    for (let shipLength=0; shipLength<size; shipLength++) {
+      $('td[data-r="'+y+'"][data-c="'+x+'"]').removeClass('water').addClass('ship');
+      x++;
+    }
+  }else{
+    for(let i=y-1;i<y+size+1;i++){
+      if($('td[data-r="'+i+'"][data-c="'+(x+1)+'"]').hasClass('ship')||$('td[data-r="'+i+'"][data-c="'+(x-1)+'"]').hasClass('ship')||$('td[data-r="'+i+'"][data-c="'+x+'"]').hasClass('ship')){
+        return false;
+      }
+    }
+    for (let shipLength=0; shipLength<size; shipLength++) {
+      $('td[data-r="'+y+'"][data-c="'+x+'"]').removeClass('water').addClass('ship');
+      y++;
+    }
+  } 
+  return true;
+}

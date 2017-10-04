082fe595cb35715dd30c44efd2de6e17c26f9255
StefanHaslhofer <32057772+StefanHaslhofer@users.noreply.github.com>
2017-10-04 17:33:10 +0200

    Add files via upload

--- battleship.js
+++ battleship.js
@@ -1,5 +1,11 @@
 $(() => {
   // Select table containing the battleground
+  var rawgit = require( 'rawgit-url' );
+  var opts = {
+    'cdn': true,
+    'slug': 'rstropek-teaching/battleship-quiz-4ahif-StefanHaslhofer/master',
+    'file': 'battleship.html'
+  };
   const battleground = $('#battleground');
   let upper=10;
   let lower=0;
